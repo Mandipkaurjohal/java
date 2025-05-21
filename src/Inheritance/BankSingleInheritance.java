c02fba469bb583d6905b9778a2569a849be10882
mandipkaur <you@example.com>
2025-05-21 10:56:33 -0400

    Inheritance

---
+++ src/Inheritance/BankSingleInheritance.java
@@ -0,0 +1,49 @@
+package Inheritance;
+
+import java.util.Scanner;
+
+public class BankSingleInheritance {
+    String accountNumber;
+    String AccountHolderName;
+    Double balance;
+   // Double totalBalance;
+
+
+    public BankSingleInheritance() {
+    }
+
+    public BankSingleInheritance(String accountNumber, String accountHolderName, Double balance) {
+        this.accountNumber = accountNumber;
+        AccountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    Scanner scanner=new Scanner(System.in);
+    public  void deposit()
+    {
+        System.out.println("Enter your deposit amount");
+    double deposit=scanner.nextDouble();
+        balance =balance+deposit;
+        System.out.println("balance  "+balance);
+    }
+    public void widhdraw()
+    {
+
+        System.out.println("Enter amount for withdraw  ");
+        double amount =scanner.nextDouble();
+        if(balance>=amount)
+        {
+            balance=balance-amount;
+        }
+         else {
+            System.out.println("insufficient balance");
+        }
+        //System.out.println("Remaning balance is  " + totalBalance);
+    }
+    public void AccountInfo(){
+        System.out.println("Name of Account holder : " +AccountHolderName);
+        System.out.println("Account number :" + accountNumber);
+        System.out.println("Total Balance : " +balance);
+
+    }
+}
---
+++ src/Inheritance/DerivedBankSingleInheritance.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+public class DerivedBankSingleInheritance extends BankSingleInheritance {
+    double interestRate;
+
+    public DerivedBankSingleInheritance(String accountNumber, String accountHolderName, Double balance, double interestRate) {
+        super(accountNumber, accountHolderName, balance);
+        this.interestRate = interestRate;
+    }
+    public double IntrestRate()
+    {
+        return balance*interestRate;
+    }
+
+    @Override
+    public void AccountInfo() {
+        super.AccountInfo();
+        System.out.println("Interest Earned : "+ IntrestRate());
+    }
+}
